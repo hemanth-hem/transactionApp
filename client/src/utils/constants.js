f79c659fa4c2da5982ced86647e2b61a0abfa5c7
hemanth-hem <hemanthhemu851321@gmail.com>
2023-04-20 03:23:11 +0530

    first commit

---
+++ client/src/utils/constants.js
@@ -0,0 +1,4 @@
+import abi from "./Transactions.json";
+
+export const contractAddress = "0xfC0x58160d63C1F7510232F51dC7333c11199fA148AbCF80344a668b72ac4Be23513F0E9E4a35C84fA";
+export const contractABI = abi.abi;
