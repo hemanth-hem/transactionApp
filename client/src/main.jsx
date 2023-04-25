3d0946742e4a0aefe21ea33be1e46d6809624d75
hemanth-hem <hemanthhemu851321@gmail.com>
2023-04-26 01:57:52 +0530

    wallet connected

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
+// import ReactDOM from "react-dom";
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+import {TransactionProvider} from './context/transactionsContext.jsx';
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <TransactionProvider>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </TransactionProvider>,
 )
